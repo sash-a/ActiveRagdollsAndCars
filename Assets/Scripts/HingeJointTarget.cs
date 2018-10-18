ca647d91e4e1eab2f9b798ca8b91752df26e4d7d
Sasha Abramowitz <reallysasha@gmail.com>
2018-10-18 22:42:43 +0200

    Added hinge joint target script and set up ragdoll targets

---
+++ Assets/Scripts/HingeJointTarget.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class HingeJointTarget : MonoBehaviour {
+ 
+    public HingeJoint hj;
+    public Transform target;
+    [Tooltip("Only use one of these values at a time. Toggle invert if the rotation is backwards.")]
+    public bool x, y, z, invert;
+     
+    void Update ()
+    {
+        if (hj != null)
+        {
+            JointSpring js;
+            js = hj.spring;
+            
+            if (x)
+            {
+                js.targetPosition = target.transform.localEulerAngles.x;
+                if (js.targetPosition > 180)
+                    js.targetPosition = js.targetPosition - 360;
+                if (invert)
+                    js.targetPosition = js.targetPosition * -1;
+ 
+                js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5);
+ 
+                hj.spring = js;
+            }
+            else if (y)
+            {
+                js.targetPosition = target.transform.localEulerAngles.y;
+                if (js.targetPosition > 180)
+                    js.targetPosition = js.targetPosition - 360;
+                if (invert)
+                    js.targetPosition = js.targetPosition * -1;
+ 
+                js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5);
+ 
+                hj.spring = js;
+            }
+            else if (z)
+            {
+                js.targetPosition = target.transform.localEulerAngles.z;
+                if (js.targetPosition > 180)
+                    js.targetPosition = js.targetPosition - 360;
+                if (invert)
+                    js.targetPosition = js.targetPosition * -1;
+ 
+                js.targetPosition = Mathf.Clamp(js.targetPosition, hj.limits.min + 5, hj.limits.max - 5);
+ 
+                hj.spring = js;
+            }
+        }
+    }
+}
